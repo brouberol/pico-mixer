72cbcd872644001d59d2f0880abd2520e157a728
Balthazar Rouberol <br@imap.cc>
2022-09-18 14:40:11 +0200

    Break down large event handler into subfunctions

--- pico_mixer_web/assets/js/script.js
+++ pico_mixer_web/assets/js/script.js
@@ -1,65 +1,93 @@
 const ws = new WebSocket("ws://localhost:8000/key_events");
 const tracksPlaying = {};
 
+function pauseAllPlayingTracks() {
+  Object.entries(tracksPlaying).forEach(([key, audioElement]) => {
+    audioElement.pause();
+    const trackProgressBar = document.getElementById(`progress_track_${key}`);
+    trackProgressBar.classList.remove("bg-success");
+    trackProgressBar.classList.add("bg-warning");
+  });
+}
+
+function unpauseAllPlayingTracks() {
+  Object.entries(tracksPlaying).forEach(([key, audioElement]) => {
+    audioElement.play();
+    const trackProgressBar = document.getElementById(`progress_track_${key}`);
+    trackProgressBar.classList.remove("bg-warning");
+    trackProgressBar.classList.add("bg-success");
+  });
+}
+
+function startTrack(trackKey, trackAudioElement, trackProgressBar) {
+  tracksPlaying[trackKey] = trackAudioElement;
+  trackProgressBar.classList.remove("bg-secondary");
+  trackProgressBar.classList.add("bg-success");
+  trackProgressBar.textContent = "100%";
+  trackAudioElement.play();
+}
+
+function stopTrack(trackKey, trackaudioElement, trackProgressBar) {
+  trackProgressBar.classList.remove("bg-success");
+  trackProgressBar.classList.add("bg-secondary");
+  trackaudioElement.pause();
+  trackaudioElement.currentTime = 0;
+  trackaudioElement.volume = 1;
+  trackProgressBar.style["width"] = '100%';
+  trackProgressBar.textContent = "";
+  delete tracksPlaying[trackKey];
+}
+
+function increaseTrackVolume(trackAudioElement, trackProgressBar) {
+  if (trackAudioElement.volume + 0.1 <= 1) {
+    trackAudioElement.volume += 0.1;
+    trackProgressBar.style["width"] = trackAudioElement.volume * 100 + "%";
+    trackProgressBar.textContent = trackProgressBar.style["width"];
+  };
+}
+
+function decreaseTrackVolume(trackAudioElement, trackProgressBar) {
+  if (trackAudioElement.volume - 0.1 > 0) {
+    trackAudioElement.volume -= 0.1;
+    trackProgressBar.style["width"] = trackAudioElement.volume * 100 + "%";
+    trackProgressBar.textContent = trackProgressBar.style["width"];
+  };
+}
+
 ws.addEventListener('message', event => {
   const keyEvent = JSON.parse(event.data);
   const usbStatus = document.getElementById("usb_status");
+
   if (keyEvent.state === "usb_disconnected") {
     usbStatus.textContent = "🚫";
   } else if (keyEvent.state === "usb_connected") {
     usbStatus.textContent = "✅";
   } else if (keyEvent.state === "pause") {
-    Object.entries(tracksPlaying).forEach(([key, audioElement]) => {
-      audioElement.pause();
-      const trackProgressBar = document.getElementById(`progress_track_${key}`);
-      trackProgressBar.classList.remove("bg-success");
-      trackProgressBar.classList.add("bg-warning");
-    });
+    pauseAllPlayingTracks();
   } else if (keyEvent.state === "unpause") {
-    Object.entries(tracksPlaying).forEach(([key, audioElement]) => {
-      audioElement.play();
-      const trackProgressBar = document.getElementById(`progress_track_${key}`);
-      trackProgressBar.classList.remove("bg-warning");
-      trackProgressBar.classList.add("bg-success");
-    });
+    unpauseAllPlayingTracks();
+  } else {
+
+    const trackProgressBar = document.getElementById(`progress_track_${keyEvent.key}`);
+    const audioElement = document.getElementById(`track_${keyEvent.key}`);
+    if (audioElement === null) {
+      return;
+    }
+
+    switch (keyEvent.state) {
+      case "on":
+        startTrack(keyEvent.key, audioElement, trackProgressBar);
+        break;
+      case "off":
+        stopTrack(keyEvent.key, audioElement, trackProgressBar);
+        break;
+      case "vol_up":
+        increaseTrackVolume(audioElement, trackProgressBar);
+        break;
+      case "vol_down":
+        decreaseTrackVolume(audioElement, trackProgressBar);
+        break;
+    }
   }
 
-  const trackProgressBar = document.getElementById(`progress_track_${keyEvent.key}`);
-  const audioElement = document.getElementById(`track_${keyEvent.key}`);
-  if (audioElement === null) {
-    return;
-  }
-  switch (keyEvent.state) {
-    case "on":
-      tracksPlaying[keyEvent.key] = audioElement;
-      trackProgressBar.classList.remove("bg-secondary");
-      trackProgressBar.classList.add("bg-success");
-      trackProgressBar.textContent = "100%";
-      audioElement.play();
-      break;
-    case "off":
-      trackProgressBar.classList.remove("bg-success");
-      trackProgressBar.classList.add("bg-secondary");
-      audioElement.pause();
-      audioElement.currentTime = 0;
-      audioElement.volume = 1;
-      trackProgressBar.style["width"] = '100%';
-      trackProgressBar.textContent = "";
-      delete tracksPlaying[keyEvent.key];
-      break;
-    case "vol_up":
-      if (audioElement.volume + 0.1 <= 1) {
-        audioElement.volume += 0.1;
-        trackProgressBar.style["width"] = audioElement.volume * 100 + "%";
-        trackProgressBar.textContent = trackProgressBar.style["width"];
-      };
-      break;
-    case "vol_down":
-      if (audioElement.volume - 0.1 > 0) {
-        audioElement.volume -= 0.1;
-        trackProgressBar.style["width"] = audioElement.volume * 100 + "%";
-        trackProgressBar.textContent = trackProgressBar.style["width"];
-      };
-      break;
-  }
 });
