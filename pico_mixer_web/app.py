bfd042bb984c1570b1616c8ece10ec9fcfe81da8
Balthazar Rouberol <br@imap.cc>
2023-06-08 11:10:54 +0200

    Perform runtime validation on the content of the sounds folder

--- pico_mixer_web/app.py
+++ pico_mixer_web/app.py
@@ -1,25 +1,54 @@
-import time
 import json
-
-import serial.serialutil
-
+import sys
+import time
 from pathlib import Path
+
+import click
+import serial.serialutil
+from flask import Flask, render_template
+from flask_sock import Sock
 from serial import Serial
 from serial.tools.list_ports import grep as list_ports
 
-from flask import Flask, render_template
-from flask_sock import Sock
-
 # From https://devicehunt.com/view/type/usb/vendor/239A
 ADAFRUIT_HARDWARE_VENDOR_ID = "239A"
+
+assets_dir = Path(__file__).parent / "assets"
+sounds_dir = assets_dir / "sounds"
 
 track_config_path = Path(__file__).parent / ".." / "config.json"
 app = Flask(
     "pico-mixer",
     static_url_path="/assets",
-    static_folder=Path(__file__).parent / "assets",
+    static_folder=assets_dir,
 )
 sock = Sock(app)
+
+if not sounds_dir.exists():
+    app.logger.warning(
+        click.style(
+            f"The {sounds_dir} folder does not exist. Creating it on the fly.",
+            fg="yellow",
+        )
+    )
+    sounds_dir.mkdir()
+
+elif not (sounds_dir.is_dir() or sounds_dir.is_symlink()):
+    app.logger.error(
+        click.style(
+            f"{sounds_dir} should be a directory and not a file. Delete it and restart the app.",
+            fg="red",
+        )
+    )
+    sys.exit(1)
+elif not list(sounds_dir.iterdir()):
+    app.logger.warning(
+        click.style(
+            f"{sounds_dir} is empty. No sounds will be played. Put sounds file under it and "
+            "update the config.json file to reflect the file names",
+            fg="yellow",
+        )
+    )
 
 
 def find_usb_device():
